59cc3eeb0372cf01a1ab5e8d4b5f24fdde995b69
kanjisaiko <hbortolomiol2@gmail.com>
2026-03-14 11:44:29 -0300

    adicionado tabela de clientes

--- cliente.py
+++ cliente.py
@@ -2,34 +2,28 @@ import socket
 import struct
 import sys
 import datetime as dt
-import time
 
-CLIENTE_IP = "0.0.0.0"
-date = dt.datetime.now()
+def main():
+    if len(sys.argv) > 1: 
+        CLIENTE_PORTA = int(sys.argv[1]) #pega o primeiro argumento da linha de comando
 
-if len(sys.argv) > 1: 
-    CLIENTE_PORTA = int(sys.argv[1]) #pega o primeiro argumento da linha de comando
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #udp
-    print(f"{date} server_addr {CLIENTE_IP}")
-    numero = int(input())
-    envio_int = struct.pack('!i', numero)
-    sock.sendto(envio_int, (CLIENTE_IP, CLIENTE_PORTA))
-    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #udp
+        CLIENTE_IP = '255.255.255.255'
+        date = dt.datetime.now()
+        print(f"{date} server_addr {CLIENTE_IP}")
+        while(True):
+            numero = int(input())
+            envio_int = struct.pack('!i', numero)
         
-        somatorio = struct.unpack('!i', sock.recv(1024))[0]
-        
-        if somatorio:
-            print(f"{date} server {CLIENTE_IP} id_req {} num_req {} total_sum {somatorio}")
-    except:
-        print("Timer estourou")
-else:
-    print("Nenhuma porta fornecida")
+            try:
+                sock.sendto(envio_int, (CLIENTE_IP, CLIENTE_PORTA)) #envia numero
 
-
-def timer() :
-    inicio = time.perf_counter()
-    time.sleep(4)
-    fim = fim = time.perf_counter()
-    tempo_decorrido = fim - inicio
-    return tempo_decorrido
-
+                data, addr = sock.recvfrom(1024) #recebe somatorio
+                id_req, num_reqs, somatorio = struct.unpack('!iii', data)
+                
+                if somatorio:
+                    print(f"{date} server {CLIENTE_IP} id_req {id_req} value {numero} num_reqs {num_reqs} total_sum {somatorio}")
+            except:
+                print("Timer estourou")
+    else:
+        print("Nenhuma porta fornecida")
--- servidor.py
+++ servidor.py
@@ -2,57 +2,71 @@ import socket
 import struct
 import sys
 import datetime as dt
+import pandas as pd
 
-IP_HOST = "0.0.0.0"
+def salvaCSV(cliente, soma_server):
+    table1 = pd.DataFrame(cliente)
+    table2 = pd.DataFrame(soma_server)
 
-#estrutura de dados de clientes mantida pelo servidor
-last_req = 0
-last_num_reqs = 0
-last_total_somatorio = 0
+    table1.to_csv('table1.csv', index=False, encoding='utf-8-sig')
+    table2.to_csv('table2.csv', index=False, encoding='utf-8-sig')
 
-#estrutura de dados da soma agregada mantida pelo servidor
-num_reqs = 0 #numero de requiscoes totais
-somatorio = 0
-id_req = 0 #id da req atual do cliente
-date = dt.datetime.now()
+def main():
+    IP_HOST = '255.255.255.255'
+    #estrutura de dados de clientes mantida pelo servidor
+    last_req = 0
 
-cliente = {}
+    #estrutura de dados da soma agregada mantida pelo servidor
+    num_reqs = 0 #numero de requiscoes totais
+    somatorio = 0
+    id_req = 0 #id da req atual do cliente
+    date = dt.datetime.now()
 
-if len(sys.argv) > 1: 
-    SERVIDOR_PORTA = int(sys.argv[1]) #pega o primeiro argumento da linha de comando
+    cliente = {'ip', 'last_req', 'last_sum'}
+    soma_server = {'num_reqs', 'total_sum'}
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((IP_HOST, SERVIDOR_PORTA))
-
-    print(f"{date} server {IP_HOST} num_reqss {num_reqs} total_somatorio {somatorio}")
-
-    while True:
-        data, addr = sock.recvfrom(1024)  
+    if len(sys.argv) > 1: 
+        SERVIDOR_PORTA = int(sys.argv[1]) #pega o primeiro argumento da linha de comando
+        #criação do socket
         try:
-            valor = struct.unpack('!i', data)[0]
-            
-            if(cliente["ip"] != addr) :
-                id_req = 1
-                cliente.update({"ip" : addr, "id_req" : id_req, "value" : valor})
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        except socket.error:
+            print('Erro ao criar socket')
+            sys.exit()
+
+        sock.bind((IP_HOST, SERVIDOR_PORTA))
+
+        print(f"{date} server {IP_HOST} num_reqss {num_reqs} total_somatorio {somatorio}")
+
+        while True:
+            try:
+                data, addr = sock.recvfrom(1024)
+                valor = struct.unpack('!i', data)[0]
                 
+                if(cliente['ip'] != addr) :
+                    id_req = 1
+                    cliente.update({'ip' : addr})
+                    
+                else: 
+                    id_req = cliente[addr]['last_req']
+                    id_req += 1
+                    
+                num_reqs += 1
+                somatorio += valor
+
+                cliente[addr] = {'last_req' : id_req, 'last_sum' : somatorio}
+                soma_server = {'num_reqs' : num_reqs, 'total_sum' : somatorio}
+
+                salvaCSV(cliente, soma_server)
+
+                print(f"{date} client {addr} id_req {id_req} value {valor} num_reqs {num_reqs} total_sum {somatorio}")
                 
-            else: 
-                id_req = cliente[addr]["id_req"]
-                id_req += 1
-                cliente[addr]["id_req"] = id_req
-                cliente[addr]["value"] = valor
-                
-            num_reqs += 1
-            somatorio += valor
-            print(f"{date} client {addr} id_req {id_req} value {valor} num_reqss {num_reqs} total_sum {somatorio}")
-            
-            envio_somatorio = struct.pack('!i', somatorio)
-            sock.sendto(somatorio, (IP_HOST, SERVIDOR_PORTA))
+                envio_somatorio = struct.pack('!iii', id_req, num_reqs, somatorio) #envia os valores para o cliente
+                sock.sendto(envio_somatorio, (IP_HOST, SERVIDOR_PORTA))
 
-        except:
-            print(f"Recebido dado nao numerico: {data}")
+            except socket.error:
+                print(f"Recebido dado nao numerico: {data}")
+                sys.exit()
 
-else:
-    print("Nenhuma porta fornecida")
-
-
+    else:
+        print("Nenhuma porta fornecida")
