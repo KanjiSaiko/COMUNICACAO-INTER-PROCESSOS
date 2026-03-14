0498e976c9ce722057a9df8be3afb0185c6015a5
vboxuser <hbortolomiol2@gmail.com>
2026-03-14 00:25:44 +0000

    adicionado requisicoes id

--- cliente.py
+++ cliente.py
@@ -2,21 +2,34 @@ import socket
 import struct
 import sys
 import datetime as dt
+import time
 
 CLIENTE_IP = "0.0.0.0"
-data = dt.datetime.now()
+date = dt.datetime.now()
 
 if len(sys.argv) > 1: 
     CLIENTE_PORTA = int(sys.argv[1]) #pega o primeiro argumento da linha de comando
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #udp
-    print(f"{data} server_addr {CLIENTE_IP}")
+    print(f"{date} server_addr {CLIENTE_IP}")
     numero = int(input())
     envio_int = struct.pack('!i', numero)
     sock.sendto(envio_int, (CLIENTE_IP, CLIENTE_PORTA))
     try:
-        ack = sock.recv(1024)
-        if ack:
-            print(f"{data} server {CLIENTE_IP} id_req {} num_req {} total_sum {}")
+        
+        somatorio = struct.unpack('!i', sock.recv(1024))[0]
+        
+        if somatorio:
+            print(f"{date} server {CLIENTE_IP} id_req {} num_req {} total_sum {somatorio}")
+    except:
+        print("Timer estourou")
 else:
-    print("Nenhum numero fornecido")
+    print("Nenhuma porta fornecida")
+
+
+def timer() :
+    inicio = time.perf_counter()
+    time.sleep(4)
+    fim = fim = time.perf_counter()
+    tempo_decorrido = fim - inicio
+    return tempo_decorrido
 
--- servidor.py
+++ servidor.py
@@ -1,22 +1,58 @@
 import socket
 import struct
+import sys
+import datetime as dt
 
 IP_HOST = "0.0.0.0"
-PORTA = 5005
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#estrutura de dados de clientes mantida pelo servidor
+last_req = 0
+last_num_reqs = 0
+last_total_somatorio = 0
 
-sock.bind((IP_HOST, PORTA))
-
+#estrutura de dados da soma agregada mantida pelo servidor
+num_reqs = 0 #numero de requiscoes totais
 somatorio = 0
+id_req = 0 #id da req atual do cliente
+date = dt.datetime.now()
 
-while True:
-    data, addr = sock.recvfrom(1024)
-    try:
-        valor = struct.unpack('!i', data)[0]
-        somatorio += valor
-        print(f"somatorio: {somatorio}")
-    except:
-        print(f"Recebido dado nao numerico: {data}")
+cliente = {}
+
+if len(sys.argv) > 1: 
+    SERVIDOR_PORTA = int(sys.argv[1]) #pega o primeiro argumento da linha de comando
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((IP_HOST, SERVIDOR_PORTA))
+
+    print(f"{date} server {IP_HOST} num_reqss {num_reqs} total_somatorio {somatorio}")
+
+    while True:
+        data, addr = sock.recvfrom(1024)  
+        try:
+            valor = struct.unpack('!i', data)[0]
+            
+            if(cliente["ip"] != addr) :
+                id_req = 1
+                cliente.update({"ip" : addr, "id_req" : id_req, "value" : valor})
+                
+                
+            else: 
+                id_req = cliente[addr]["id_req"]
+                id_req += 1
+                cliente[addr]["id_req"] = id_req
+                cliente[addr]["value"] = valor
+                
+            num_reqs += 1
+            somatorio += valor
+            print(f"{date} client {addr} id_req {id_req} value {valor} num_reqss {num_reqs} total_sum {somatorio}")
+            
+            envio_somatorio = struct.pack('!i', somatorio)
+            sock.sendto(somatorio, (IP_HOST, SERVIDOR_PORTA))
+
+        except:
+            print(f"Recebido dado nao numerico: {data}")
+
+else:
+    print("Nenhuma porta fornecida")
 
 
