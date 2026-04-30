71338936e52fee4451015d07f6d457787840cd7f
KanjiSaiko <hbortolomiol2@gmail.com>
2026-04-30 15:35:42 -0300

    funcionalidade para executar multiplos clientes na mesma maquina

--- processamento.py
+++ processamento.py
@@ -3,6 +3,7 @@ import struct
 import interface
 import threading
 import datetime as dt
+import time
 
 def processamento_server(sock, num_reqs, somatorio):
     #estrutura: {'address' : address, 'last_req': id_req, 'last_num_reqs' : last_num_reqs, 'last_sum': somatorio}
@@ -13,26 +14,28 @@ def processamento_server(sock, num_reqs, somatorio):
             message, addr = sock.recvfrom(1024)
             ip_client = addr[0]
 
-            # 1. É um cliente novo pedindo descoberta? (Checa se é a string de 8 bytes)
+            # É um cliente novo pedindo descoberta? (Checa se é a string de 8 bytes)
             if message == b"discover":
                 sock.sendto(b"ack_discover", addr)
-                # Pode aproveitar e já criar a entrada dele na tabela_1 aqui, se quiser!
                 continue # Volta para o topo do loop para escutar a próxima mensagem
 
-            # 2. É um pacote de dados? (Checa se tem o tamanho exato do struct !iQ)
+            # É um pacote de dados? (Checa se tem o tamanho exato do struct !iQ)
             elif len(message) == 12:
                 id_req, data = struct.unpack('!iQ', message)
             
-                if(ip_client in tabela_1 and tabela_1[ip_client]['last_req'] == id_req): #DUPLICADA
+                if(addr in tabela_1 and tabela_1[addr]['last_req'] == id_req): #DUPLICADA
                     date = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                     print(f"{date} client {addr} DUP!! id_req {id_req} value {data} num_reqs {num_reqs} total_sum {somatorio}")
-                    envio_somatorio = tabela_1[ip_client]['last_sum']
+
+                    envio_somatorio = tabela_1[addr]['last_sum']
+                    envio_num_reqs = tabela_1[addr]['last_num_reqs']
+                    envio_id = tabela_1[addr]['last_req']
                     
-                    sock.sendto(struct.pack('!iiQ', id_req, num_reqs, envio_somatorio), addr)
+                    sock.sendto(struct.pack('!iiQ', envio_id, envio_num_reqs, envio_somatorio), addr)
 
                 else:
-                    if (ip_client not in  tabela_1) : #CASO IP NAO ESTEJA NA TABELA
-                        tabela_1[ip_client] = {
+                    if (addr not in  tabela_1) : #CASO IP NAO ESTEJA NA TABELA
+                        tabela_1[addr] = {
                             'address': ip_client,
                             'last_req': None,
                             'last_num_reqs': 0,
@@ -42,9 +45,9 @@ def processamento_server(sock, num_reqs, somatorio):
                     num_reqs += 1
                     somatorio += data
 
-                    tabela_1[ip_client]['last_req'] = id_req
-                    tabela_1[ip_client]['last_num_reqs'] = num_reqs
-                    tabela_1[ip_client]['last_sum'] = somatorio
+                    tabela_1[addr]['last_req'] = id_req
+                    tabela_1[addr]['last_num_reqs'] = num_reqs
+                    tabela_1[addr]['last_sum'] = somatorio
 
 
                     tabela_2 = {'num_reqs' : num_reqs, 'total_sum' : somatorio}
@@ -52,6 +55,14 @@ def processamento_server(sock, num_reqs, somatorio):
                     interface.interface_server(ip_client, id_req, data, tabela_2)
 
                     envio_somatorio = struct.pack('!iiQ', id_req, num_reqs, somatorio) #envia os valores para o cliente
+
+                    # ---> INÍCIO DA SIMULAÇÃO DE DUPLICADA <---
+                    # Força o servidor a atrasar o ACK da requisição número 3
+                    #if id_req == 3:
+                    #    print("Simulando rede lenta... segurando o ACK!")
+                    #    time.sleep(1.0) # Dorme 1 segundo (O cliente estoura com 0.2s)
+                    # ---> FIM DA SIMULAÇÃO DE DUPLICADA <---
+
                     sock.sendto(envio_somatorio, addr)
     
         except socket.error:
@@ -68,7 +79,7 @@ def processamento_cliente(sock, CLIENTE_IP, CLIENTE_PORTA):
     thread_ouvinte = threading.Thread(
         target=ouvinte_servidor, 
         args=(sock, estado_atual, evento),
-        daemon=True) # Garante que a thread morra se o programa fechar)
+        daemon=True) # Garante que a thread morra se o programa fechar
     thread_ouvinte.start()
 
     while(True):
