95e1738364aa6e6473a03abfa0400cb1ddc417c7
KanjiSaiko <hbortolomiol2@gmail.com>
2026-05-02 15:00:54 -0300

    funcionando para 1mi de req

--- processamento.py
+++ processamento.py
@@ -20,19 +20,23 @@ def processamento_server(sock, num_reqs, somatorio):
                 sock.sendto(b"ack_discover", addr)
                 continue # Volta para o topo do loop para escutar a próxima mensagem
 
-            # É um pacote de dados? (Checa se tem o tamanho exato do struct !iQ)
             elif len(message) == 12:
                 id_req, data = struct.unpack('!iQ', message)
-            
-                if(addr in tabela_1 and tabela_1[addr]['last_req'] == id_req): #DUPLICADA
-                    date = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                    print(f"{date} client {ip_client} DUP!! id_req {id_req} value {data} num_reqs {num_reqs} total_sum {somatorio}")
-
-                    envio_somatorio = tabela_1[addr]['last_sum']
-                    envio_num_reqs = tabela_1[addr]['last_num_reqs']
-                    envio_id = tabela_1[addr]['last_req']
+                # Verifica se o cliente existe e se o pacote é <= ao último processado (Pacotes velhos/Duplicatas)
+                if addr in tabela_1 and tabela_1[addr]['last_req'] is not None and id_req <= tabela_1[addr]['last_req']:
                     
-                    sock.sendto(struct.pack('!iiQ', envio_id, envio_num_reqs, envio_somatorio), addr)
+                    if(tabela_1[addr]['last_req'] == id_req): #DUPLICADA
+                        date = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                        print(f"{date} client {ip_client} DUP!! id_req {id_req} value {data} num_reqs {num_reqs} total_sum {somatorio}")
+
+                        envio_somatorio = tabela_1[addr]['last_sum']
+                        envio_num_reqs = tabela_1[addr]['last_num_reqs']
+                        envio_id = tabela_1[addr]['last_req']
+                        
+                        sock.sendto(struct.pack('!iiQ', envio_id, envio_num_reqs, envio_somatorio), addr)
+
+                    else:
+                        pass
 
                 else:
                     if (addr not in  tabela_1) : #CASO IP NAO ESTEJA NA TABELA
@@ -50,20 +54,11 @@ def processamento_server(sock, num_reqs, somatorio):
                     tabela_1[addr]['last_num_reqs'] = num_reqs
                     tabela_1[addr]['last_sum'] = somatorio
 
-
                     tabela_2 = {'num_reqs' : num_reqs, 'total_sum' : somatorio}
 
                     interface.interface_server(ip_client, id_req, data, tabela_2)
 
                     envio_somatorio = struct.pack('!iiQ', id_req, num_reqs, somatorio) #envia os valores para o cliente
-
-                    # ---> INÍCIO DA SIMULAÇÃO DE DUPLICADA <---
-                    # Força o servidor a atrasar o ACK da requisição número 3
-                    #if id_req == 3:
-                    #    print("Simulando rede lenta... segurando o ACK!")
-                    #    time.sleep(1.0) # Dorme 1 segundo (O cliente estoura com 0.2s)
-                    # ---> FIM DA SIMULAÇÃO DE DUPLICADA <---
-
                     sock.sendto(envio_somatorio, addr)
     
         except socket.error:
